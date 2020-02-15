044f096a9765b545f349cc8309a27fcb5d19573e
Timothy Morland <thetimmorland@gmail.com>
2020-02-15 13:08:13 -0500

    Updates

---
+++ precache-manifest.faadd15db4b815ed472fba7a5c30a93b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd91f7e77b480ca35d9e9ff93c3cae3",
+    "url": "/resistor-site/index.html"
+  },
+  {
+    "revision": "24b9b9484d878480f341",
+    "url": "/resistor-site/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "29145d04ef479f248158",
+    "url": "/resistor-site/static/js/2.1b494354.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/resistor-site/static/js/2.1b494354.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24b9b9484d878480f341",
+    "url": "/resistor-site/static/js/main.b33f3192.chunk.js"
+  },
+  {
+    "revision": "7ca32541a161c312d673",
+    "url": "/resistor-site/static/js/runtime-main.40a2bc6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resistor-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
