72e78b9d58657a900b9250974b88125191de6f70
Timothy Morland <thetimmorland@gmail.com>
2020-02-15 16:42:18 -0500

    setup base react app with webcam, confirm opencv is working as intended

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import cv from "./opencv";
 
-function App() {
+import Webcam from "react-webcam";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  LinearProgress,
+  Container,
+  MenuItem,
+  Grid,
+  Paper,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
+
+cv["onRuntimeInitialized"] = () => {
+  console.log(cv.getBuildInformation());
+};
+
+export default function App() {
+  const webcamRef = React.useRef(null);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+  }, [webcamRef]);
+
+  const [resistorFound, updateResistorFound] = React.useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="sticky">
+        <Toolbar>
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            justify="space-between"
+          >
+            <Grid item>
+              <Typography variant="h6">Resistor site️</Typography>
+            </Grid>
+            <Grid item>
+              <MenuItem
+                onClick={() => {
+                  window.location.href =
+                    "https://github.com/thetimmorland/resistor-site";
+                }}
+              >
+                <GitHubIcon />
+              </MenuItem>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <Box m={1}>
+        <Container maxWidth="sm">
+          <Paper>
+            <Box p={1}>
+              <Webcam
+                audio={false}
+                ref={webcamRef}
+                width="100%"
+                screenshotFormat="image/jpeg"
+              />
+              <LinearProgress />
+            </Box>
+          </Paper>
+        </Container>
+      </Box>
+    </>
   );
 }
-
-export default App;
